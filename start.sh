0d5b084d77f79944be5d14994d26d321ae256074
Rodrigo Navarro <r0n0066@M-C02ZQ1K2MD6P.local>
2021-04-16 09:38:43 -0400

    feat(core): initialization script

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+graceful_exit() {
+    echo "Exit requested..."
+    docker-compose down
+    exit 0
+}
+# this line exectues when process exits
+trap 'graceful_exit' TERM INT HUP
+
+echo "Inicializando ambiente..."
+docker compose up -d
+
+echo "Esperando por Zepellin..."
+http_response=0
+until [[ $http_response == "200" ]]; do http_response=$(curl -i -s localhost:8080 | grep HTTP | awk '{print $2}') && echo "..." && sleep 2; done
+
+echo "loading workshop"
+curl http://localhost:8080/api/notebook/import -d @'flink-kafka-workshop1.zpln'
+
+printf "\n\nPresione enter para detener la ejecución"
+read -r -n 1
+
+echo "Bajando ambiente..."
+docker compose down
+echo "Done! happy coding"
