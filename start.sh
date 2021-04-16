255881a5babf1bcca6a4f86d93b49de51e6b6708
Rodrigo Navarro <r0n0066@M-C02ZQ1K2MD6P.local>
2021-04-16 09:40:14 -0400

    feat(notebook): add notebook with initial steps [WIP]

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ http_response=0
 until [[ $http_response == "200" ]]; do http_response=$(curl -i -s localhost:8080 | grep HTTP | awk '{print $2}') && echo "..." && sleep 2; done
 
 echo "loading workshop"
-curl http://localhost:8080/api/notebook/import -d @'flink-kafka-workshop1.zpln'
+curl http://localhost:8080/api/notebook/import -d @'flink-kafka-workshop.zpln'
 
 printf "\n\nPresione enter para detener la ejecución"
 read -r -n 1
