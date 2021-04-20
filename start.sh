b6473f15db880630a556f52d1f1dc677c60a1700
Ricardo Gaete <ricardo.gaete@walmart.com>
2021-04-20 15:46:10 -0400

    Adding a go producer

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ echo "Esperando por Zepellin..."
 http_response=0
 until [[ $http_response == "200" ]]; do http_response=$(curl -i -s localhost:8080 | grep HTTP | awk '{print $2}') && echo "..." && sleep 2; done
 
-echo "loading workshop"
+echo "Cargando workshop"
 curl http://localhost:8080/api/notebook/import -d @'flink-kafka-workshop.zpln'
 
 printf "\n\nPresione enter para detener la ejecución"
@@ -23,4 +23,4 @@ read -r -n 1
 
 echo "Bajando ambiente..."
 docker compose down
-echo "Done! happy coding"
+echo "Listo! happy coding"
